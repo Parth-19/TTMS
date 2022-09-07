db45a5f861be5009e87f6a42728c0d3d24bfa40b
Parth Maheshwari <74190895+Parth-19@users.noreply.github.com>
2022-09-07 23:26:21 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+$ heroku buildpacks
+=== my-example-app Buildpack URLs
+1. heroku-community/example-buildpack
+2. https://github.com/heroku/heroku-buildpack-python.git
+$ heroku buildpacks:clear
+$ heroku buildpacks:add heroku-community/example-buildpack
+$ heroku buildpacks:add heroku/python
+$ heroku buildpacks
+=== my-example-app Buildpack URLs
+1. heroku-community/example-buildpack
+2. heroku/python
