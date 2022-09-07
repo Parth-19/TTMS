ed242e0e28a3b54a1c1ae8464d0b44a5401456da
Parth Maheshwari <74190895+Parth-19@users.noreply.github.com>
2022-09-07 23:36:49 +0530

    trial

--- setup.py
+++ setup.py
@@ -1,11 +1,2 @@
-$heroku buildpacks
-=== my-example-app Buildpack URLs
-1. heroku-community/example-buildpack
-2. https://github.com/heroku/heroku-buildpack-python.git
-$heroku buildpacks:clear
-$heroku buildpacks:add heroku-community/example-buildpack
-$heroku buildpacks:add heroku/python
-$heroku buildpacks
-=== my-example-app Buildpack URLs
-1. heroku-community/example-buildpack
-2. heroku/python
+git push heroku master
+-----> Python app detected
